0ae2fe589156034ad13762f07bf877f7b4f4e16b
Claudio Toro <ctoro@lotus.cl>
2018-05-24 16:06:13 -0400

    recibe xml listo

---
+++ src/main/java/com/mycompany/rest/GetQuoteResource.java
@@ -0,0 +1,16 @@
+package com.mycompany.rest;
+
+import javax.ws.rs.*;
+
+@Path("/restservice/")
+public class GetQuoteResource {
+	
+	@POST
+	@Path("/getQuote/")
+	@Produces("application/json")
+	public String getQuote(String input) {
+		return null;
+	}
+	
+
+}
